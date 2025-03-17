6c5f27cdbc697855c73433b35561b8d02f1ba503
Eierkopp <depperter.depp@googlemail.com>
2025-03-17 10:39:17 +0100

    imageio references removed

--- setup.py
+++ setup.py
@@ -4,21 +4,13 @@ from setuptools import setup
 
 setup(
     name="viddup",
-    version="2.3",
+    version="2.5",
     description="Simple video hashing and similarity detection tool",
     author="Eierkopp",
     packages=["viddup"],
     # scripts=["bin/viddup"],
     author_email="depp@eierkopp.ddnss.org",
     install_requires=[
-        #        "annoy",
-        #        "imageio",
-        "imageio-ffmpeg",
-        #        "psycopg",
-        #        "psycopg-pool",
-        #        "pyaml",
-        #        "pygobject",
-        #        "scipy",
-        #        "tqdm",
+        "annoy",
     ],
 )
